e1f0e3f481958e2292fdc667450647f2c4f99c45
Ariel Barreiro <abarrei@gmail.com>
2016-07-18 11:06:59 -0300

    Completing other files

--- src/main/java/ar/uba/fi/fallas/App.java
+++ src/main/java/ar/uba/fi/fallas/App.java
@@ -1,13 +1,37 @@
 package ar.uba.fi.fallas;
 
+import javax.inject.Inject;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+import org.kie.api.cdi.KSession;
+import org.kie.api.runtime.KieSession;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    @Inject
+    @KSession()
+    private KieSession kSession;
+
+    public void bootstrapDrools() {
+        // The KieSession was injected so we can use it now
+        kSession.insert("Hi There!");
+        int rulesFired = kSession.fireAllRules();
+        System.out.println("Rules Fired: "+rulesFired);
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //Boostrap the CDI container, in this case WELD
+        Weld w = new Weld();
+
+        WeldContainer wc = w.initialize();
+        App app = wc.instance().select(App.class).get();
+        app.bootstrapDrools();
+
+        w.shutdown();
     }
 }
